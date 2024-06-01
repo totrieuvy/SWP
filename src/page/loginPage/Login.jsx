cce3790bfa2fe502c877ce21149ed4e4086fd4fa
totrieuvy <vytotrieu12a121@gmail.com>
2024-06-01 22:05:20 +0700

    login with google

--- src/page/loginPage/Login.jsx
+++ src/page/loginPage/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Children, useEffect } from "react";
 import { Button, Form, Input, notification } from "antd";
 import "./Login.css";
 import { Link, useNavigate } from "react-router-dom";
@@ -7,6 +7,8 @@ import { useForm } from "antd/es/form/Form";
 import api from "../../config/axios";
 import { useDispatch } from "react-redux";
 import { login } from "../../redux/features/counterSlice";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth, googleProvider } from "../../config/firebase";
 
 function Login() {
   const [formVariable] = useForm();
@@ -56,7 +58,49 @@ function Login() {
 
   useEffect(() => {
     document.title = "Login";
-  });
+  }, []);
+
+  const handleLoginGoogle = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(async (result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        console.log(credential);
+
+        const response = await api.post("/account/loginGG", {
+          token: result.user.accessToken,
+        });
+        console.log(response);
+        let token = response.data.token;
+        let role = response.data.role;
+        localStorage.setItem("token", token);
+        console.log(response);
+        console.log(response.data);
+        dispatch(login(response.data));
+        if (role === "ROLE_ADMIN") {
+          notification.success({
+            message: "Thành công",
+            description: "Đăng nhập thành công",
+          });
+          navigate("/admin");
+        } else if (role === "ROLE_MANAGER") {
+          notification.success({
+            message: "Thành công",
+            description: "Đăng nhập thành công",
+          });
+          navigate("/manager");
+        } else if (role === "ROLE_STAFF") {
+          notification.success({
+            message: "Thành công",
+            description: "Đăng nhập thành công",
+          });
+          navigate("/staff");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="Login">
@@ -105,8 +149,8 @@ function Login() {
               </Form.Item>
             </Form>
             <p className="login__form__left__email">Or login with Google</p>
-            <button className="login__form__left__google">
-              <img src="../public/images/google.png" alt="Google" />
+            <button className="login__form__left__google" onClick={handleLoginGoogle}>
+              <img src="/images/google.png" alt="Google" />
               <p>Google</p>
             </button>
           </div>
