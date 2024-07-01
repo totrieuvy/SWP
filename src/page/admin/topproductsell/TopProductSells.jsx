8e52ba5e36859e55bf3fbc32f4112f76b7982233
totrieuvy <vytotrieu12a121@gmail.com>
2024-07-01 19:50:36 +0700

    manager xem promotion nào còn hạn, nào hết hạn

--- src/page/admin/topproductsell/TopProductSells.jsx
+++ src/page/admin/topproductsell/TopProductSells.jsx
@@ -3,7 +3,6 @@ import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 import api from "../../../config/axios";
 import { Spin } from "antd";
-// import "./TopProduct.scss";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
