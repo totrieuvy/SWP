b673cd713fda2215fdf3bf7ab3484d2256ebd334
totrieuvy <vytotrieu12a121@gmail.com>
2024-07-04 11:01:55 +0700

    temp

--- src/page/staff/staff-profile/StaffProfile.jsx
+++ src/page/staff/staff-profile/StaffProfile.jsx
@@ -1,4 +1,4 @@
-import { Table } from "antd";
+import { Button, Modal, Table, Form, Input } from "antd";
 import api from "../../../config/axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
@@ -7,6 +7,10 @@ import { selectUser } from "../../../redux/features/counterSlice";
 function StaffProfile() {
   const [dataSource, setDataSource] = useState([]);
   const user = useSelector(selectUser);
+  const [visible, setVisible] = useState(false);
+  const [oldData, setOldData] = useState({});
+  const [form] = Form.useForm();
+
   const fetchStaffProfile = async () => {
     try {
       const response = await api.get(`/api/staff-accounts/${user.id}`);
@@ -56,10 +60,62 @@ function StaffProfile() {
       dataIndex: "email",
       key: "email",
     },
+    {
+      title: "Cập nhật",
+      dataIndex: "staffID",
+      key: "staffID",
+      render: (staffID, record) => (
+        <Button
+          type="primary"
+          onClick={() => {
+            handleOpenModal(record);
+          }}
+        >
+          Cập nhật
+        </Button>
+      ),
+    },
   ];
+
+  const handleOpenModal = (record) => {
+    setOldData(record);
+    form.setFieldsValue(record);
+    setVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setVisible(false);
+    form.resetFields();
+  };
+
+  const handleUpdate = async (values) => {
+    try {
+      await api.put(`/api/staff-accounts/${user.id}`, values);
+      const data = await fetchStaffProfile();
+      const dataArray = Array.isArray(data) ? data : [data];
+      setDataSource(dataArray);
+      handleCloseModal();
+    } catch (error) {
+      console.error("Error updating staff profile:", error);
+    }
+  };
+
   return (
     <div className="StaffProfile">
       <Table dataSource={dataSource} columns={columns} />
+      <Modal title="Cập nhật thông tin" visible={visible} onCancel={handleCloseModal} onOk={() => form.submit()}>
+        <Form form={form} layout="vertical" onFinish={handleUpdate}>
+          <Form.Item name="username" label="Tên">
+            <Input />
+          </Form.Item>
+          <Form.Item name="phoneNumber" label="Số điện thoại">
+            <Input />
+          </Form.Item>
+          <Form.Item name="email" label="Email">
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 }
